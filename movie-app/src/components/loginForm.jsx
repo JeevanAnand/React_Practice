2df713eb3e62c6b29b12300cbda906f8f89563ce
JeevanAnand <jeev46@gmail.com>
2021-04-26 10:08:38 +0530

    handling form submit

--- movie-app/src/components/loginForm.jsx
+++ movie-app/src/components/loginForm.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LoginForm extends Component {
-    render() { 
-        return (
-            <h1>This is Login form</h1>
-          );
-    }
+    
+handleSubmmit = fobj =>{
+    fobj.preventDefault();
+    console.log("Handling form submit ");
+
 }
- 
+    
+  render() {
+    return (
+      <div>
+        <h1>This is Login form</h1>
+        <form onSubmit={this.handleSubmmit}>
+          <div className="form-group">
+            <label htmlFor="username">User Name</label>
+            <input type="text" id="username" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="text"  id="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 export default LoginForm;
